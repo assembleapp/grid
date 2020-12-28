5daa3e68a80ec4c8a178cf052cfdb1c740912323
grace <grace.c.youngblood@gmail.com>
2020-12-28 13:27:30 -0500

    add basic grid; see https://github.com/adazzle/react-data-grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Grid from "react-data-grid"
+import "react-data-grid/dist/react-data-grid.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header>
+        <Source
+        source="https://assemble-opposed.herokuapp.com/v1/graphql"
+        schema={{companies: { _: ['name', 'address'], danger: 'number?', labels: 'symbols?' }}}
         >
-          Learn React
-        </a>
+          {/* {model => ( */}
+            <Grid
+            columns={[
+              {key: 'id', name: 'ID'},
+              {key: 'title', name: 'Title'},
+            ]}
+            rows={[
+              { id: 0, title: 'Example'},
+              { id: 1, title: 'Demo'},
+            ]}
+            />
+          {/* )} */}
+        </Source>
       </header>
     </div>
   );
 }
+
+const Source = ({children}) => <div>{children}</div>
 
 export default App;
