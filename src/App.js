9b3681e3488dbfab94d100e4822eaca72d1421eb
grace <grace.c.youngblood@gmail.com>
2020-12-30 13:14:12 -0500

    add styled-components.

--- src/App.js
+++ src/App.js
@@ -2,30 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import Grid from "react-data-grid"
 import "react-data-grid/dist/react-data-grid.css"
+import styled from "styled-components"
 
 import Scope from "./hierarch/scope"
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Scope
-        source="https://assemble-opposed.herokuapp.com/v1/graphql"
-        schema={{companies: { _: ['name', 'address'], danger: 'number?', labels: 'symbols?' }}}
-        >
-          {model => (
-            <Grid
-            columns={[
-              {key: 'name', name: 'Name'},
-              {key: 'address', name: 'Address'},
-            ]}
-            rows={model.companies.toJSON()}
-            />
-          )}
-        </Scope>
-      </header>
-    </div>
+    <Layout>
+      <Scope
+      source="https://assemble-opposed.herokuapp.com/v1/graphql"
+      schema={{companies: { _: ['name', 'address'], danger: 'number?', labels: 'symbols?' }}}
+      >
+        {model => (
+          <Grid
+          columns={[
+            {key: 'name', name: 'Name'},
+            {key: 'address', name: 'Address'},
+          ]}
+          rows={model.companies.toJSON()}
+          />
+        )}
+      </Scope>
+    </Layout>
   );
 }
+
+const Layout = styled.div`
+height: 100vh;
+`
 
 export default App;
